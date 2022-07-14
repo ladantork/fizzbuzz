c5d827aece81950b305e7cfce25670236ef827ac
ladantork <ladantorkzaban0@gmail.com>
2022-07-14 11:28:29 +0100

    Refactor

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,15 +1,7 @@
 def fizzbuzz(num)
-  # return "fizzbuzz" when num is multiple of 3 and 5
-  if num % 3 < 1 && num % 5 < 1
-    return "fizzbuzz"
-  # return "fizz" when num is multiple of 3
-  elsif num % 3 < 1
-    return "fizz"
-  # return "buzz" when num is multiple of 5
-  elsif num % 5 < 1
-    return "buzz"
-  # return the number in all other cases
-  else
-    return num
-  end
+  return "fizzbuzz" if num % 15 < 1
+  return "fizz" if num % 3 < 1
+  return "buzz" if num % 5 < 1
+  return num
+
 end
